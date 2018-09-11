c02726ce3bbfca030a0a749823f9d255a48cac08
Maxim Danilov <danilov.ms.dev@gmail.com>
2018-09-11 16:48:47 +0300

    update(grammars): format code

--- _chrome.py
+++ _chrome.py
@@ -11,63 +11,64 @@ from aenea import (
     MappingRule,
     ProxyAppContext,
     Text
-    )
+)
 
 chromium_context = aenea.AeneaContext(
     ProxyAppContext(match='regex', title='(?i).*chrome.*'),
     AppContext(title='chrome')
-    )
+)
 
 chromium_grammar = Grammar('chromium', context=chromium_context)
 
 
 class ChromiumRule(MappingRule):
     mapping = aenea.configuration.make_grammar_commands('chromium', {
-        'open frame':                        Key('c-t'),
-        'close frame [<n>]':                 Key('c-w:%(n)d'),
-        'frame [<n>]':                       Key('c-%(n)d'),
-        'frame left [<n>]':                  Key('cs-tab:%(n)d'),
-        'frame right [<n>]':                 Key('c-tab:%(n)d'),
+        'open frame': Key('c-t'),
+        'close frame [<n>]': Key('c-w:%(n)d'),
+        'frame [<n>]': Key('c-%(n)d'),
+        'frame left [<n>]': Key('cs-tab:%(n)d'),
+        'frame right [<n>]': Key('c-tab:%(n)d'),
 
         # url
-        'search [<text>]':                   Key('c-k') + Text('%(text)s'),
-        'address':                           Key('c-l'),
+        'search [<text>]': Key('c-k') + Text('%(text)s'),
+        'address': Key('c-l'),
 
         # find
-        'voice search':                      Key('c-backslash'),
-        'find [<text>]':                     Key('c-f') + Text('%(text)s'),
-        'next [<n>]':                        Key('c-g:%(n)d'),
-        'previous [<n>]':                    Key('cs-g:%(n)d'),
+        'voice search': Key('c-backslash'),
+        'find [<text>]': Key('c-f') + Text('%(text)s'),
+        'next [<n>]': Key('c-g:%(n)d'),
+        'previous [<n>]': Key('cs-g:%(n)d'),
 
-        'history':                           Key('c-h'),
-        'bookmarks':                         Key('c-h'),
+        'history': Key('c-h'),
+        'bookmarks': Key('c-h'),
 
         # navigation
-        'reload':                            Key('c-r'),
+        'reload': Key('c-r'),
         # 'back [<n>]':                      Key('a-left:%(n)d'), -> to global
-        'forward [<n>]':                     Key('a-right:%(n)d'),
+        'forward [<n>]': Key('a-right:%(n)d'),
 
         # vimium
-        'link':                              Key('f'),
-        'new link':                          Key('s-f'),           
-        'number [<n>]':                      Text('%(n)d'),
+        'link': Key('f'),
+        'new link': Key('s-f'),
+        'number [<n>]': Text('%(n)d'),
 
         # zoom
-        'zoom in [<n>]':                     Key('c-plus:%(n)d'),           
-        'zoom out [<n>]':                    Key('c-minus:%(n)d'),           
+        'zoom in [<n>]': Key('c-plus:%(n)d'),
+        'zoom out [<n>]': Key('c-minus:%(n)d'),
 
         # scroll slow
-        'slide':                             Text('}'),
+        'slide': Text('}'),
 
         # open address
         # 'open google translate':             Key('c-k') + Text('https://translate.google.ru/?hl=ru#en/ru/'),
-        })
+    })
 
     extras = [IntegerRef('n', 1, 100), Dictation('text')]
     defaults = {
         'n': 1,
         'text': ''
-        }
+    }
+
 
 chromium_grammar.add_rule(ChromiumRule())
 
