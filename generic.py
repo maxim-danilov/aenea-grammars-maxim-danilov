dd26220dc3909444d1a7afe2f5215663c21fc5af
danilov.ms.dev <danilov-ms-dev>
2018-09-20 11:13:13 +0300

    update(grammars): alphabet, numbers, remove unusable keystrokes, cleanup code

--- generic.py
+++ generic.py
@@ -11,6 +11,7 @@ from words import (
 )
 import re
 
+
 def cancel_and_sleep(text=None, text2=None):
     """Used to cancel an ongoing dictation and puts microphone to sleep.
     This method notifies the user that the dictation was in fact canceled,
@@ -22,41 +23,42 @@ def cancel_and_sleep(text=None, text2=None):
     print("* Dictation canceled. Going to sleep. *")
     setMicState("sleeping")
 
+
 letterMap = {
-    "(alpha|arch)": "a",
-    "(bravo|brav) ": "b",
-    "(charlie|turley|char) ": "c",
-    "(delta|del) ": "d",
-    "(echo|eck) ": "e",
-    "(foxtrot|fox) ": "f",
-    "(golf|gang) ": "g",
-    "(hotel) ": "h",
-    "(india|indigo|ish) ": "i",
-    "(juliet|julia) ": "j",
-    "(kilo) ": "k",
-    "(lima|lion|line|lie) ": "l",
-    "(mike) ": "m",
-    "(november|noy) ": "n",
-    "(Oscar|osh) ": "o",
-    "(papa|poppa|pom) ": "p",
-    "(quebec|quiche|queen) ": "q",
-    "(romeo|ree) ": "r",
-    "(sierra|soy) ": "s",
-    "(tango|tay) ": "t",
-    "(uniform|umm) ": "u",
-    "(victor|van) ": "v",
-    "(whiskey|wes) ": "w",
-    "(x-ray) ": "x",
-    "(yankee|yaa) ": "y",
-    "(zulu) ": "z",
+    "alpha": "a",
+    "bravo": "b",
+    "charlie": "c",
+    "delta": "d",
+    "echo": "e",
+    "foxtrot": "f",
+    "golf": "g",
+    "hotel ": "h",
+    "india": "i",
+    "juliet": "j",
+    "kilo ": "k",
+    "lima": "l",
+    "mike ": "m",
+    "november": "n",
+    "Oscar": "o",
+    "papa": "p",
+    "quebec": "q",
+    "romeo": "r",
+    "sierra": "s",
+    "tango": "t",
+    "uniform": "u",
+    "victor": "v",
+    "whiskey": "w",
+    "x-ray ": "x",
+    "yankee": "y",
+    "zulu ": "z",
 }
 
 numberMap = {
     "zero": "0",
     "one": "1",
-    "duo": "2",
+    "two": "2",
     "three": "3",
-    "quad": "4",
+    "four": "4",
     "five": "5",
     "six": "6",
     "seven": "7",
@@ -114,14 +116,6 @@ modifierMap = {
     "command|cod": "w",
 }
 
-# Modifiers for the press-command, if only the modifier is pressed.
-singleModifierMap = {
-    "alt|option": "a",
-    "control": "c",
-    "shift": "s",
-    "command|cod": "w",
-}
-
 pressKeyMap = {}
 pressKeyMap.update(letterMap)
 pressKeyMap.update(numberMap)
@@ -130,14 +124,6 @@ pressKeyMap.update(specialCharMap)
 
 # These words can be prefaced by the word "say"
 reservedWord = {
-    "PDF-lay-tech": "pdflatex",
-    "(bib-teck|big-tech)": "bibtex",
-    "G G plot": "ggplot",
-    "dee plyer": "dplyr",
-    "geom point": "geom_point",
-    "geom line": "geom_line",
-    "jay store": "JSTOR ",
-
     "up": "up ",
     "down": "down ",
     "left": "left ",
@@ -164,7 +150,6 @@ reservedWord = {
     "dot": "dot ",
     "period": "period ",
     "minus": "minus ",
-    "semi": "semi ",
     "hyphen": "hyphen ",
     "kill": "kill ",
     "to end": "to end ",
@@ -180,50 +165,7 @@ reservedWord = {
 nonVimGenericKeys = {
     "say <text>": Function(lower_that),
     "cap <text>": Function(cap_that),
-
-    "up [<n>]": Key("up:%(n)d"),
-    "down [<n>]": Key("down:%(n)d"),
-    "left [<n>]": Key("left:%(n)d"),
-    "right [<n>]": Key("right:%(n)d"),
-
-    "(undo|scratch) [<n>]": Key("w-z:%(n)d"),
-    "redo [<n>]": Key("sw-z:%(n)d"),
-    "kill [<n>]": Key("del:%(n)d"),
-    "kill to end": Key("c-k"),
-    "cut line": Key("c-a, sw-right, w-x, w-right, del"),
-    "copy line": Key("w-left, sw-right, w-c, w-right"),
-    "copy [that]": Key("w-c"),
-    "cut [that]": Key("w-x"),
-    "paste [that]": Key("w-v"),
-
-    # "home": Key("w-left"),
-    # "end": Key("w-right"),
-    "go to top": Key("w-up"),
-    "go to bottom": Key("w-down"),
-
-    "dupe line": Key("w-left, ws-right, w-c, w-right, enter, w-v"),
-    "sell line": Key("w-left, ws-right"),
-    'sell to start': Key('ws-left'),
-    'sell to end': Key('ws-right'),
-    'sell to top': Key('ws-up'),
-    'sell to bottom': Key('ws-down'),
-
-    # "bit [<n>]": Key("a-left:%(n)d"),
-    # "fit [<n>]": Key("a-right:%(n)d"),
-    # "de-bit [<n>]": Key("sa-left:%(n)d") + Key("del"),
-    # "de-fit [<n>]": Key("sa-right:%(n)d") + Key("del"),
-
-    "save file": Key("w-s"),
-    "space [repeat <n>]": Key("space:%(n)d"),
-    "<letters>": Key("%(letters)s"),
-    "sky <letters>": Key("s-%(letters)s"),
-    "num <numbers>": Key("%(numbers)s"),
-    "<numbers>": Key("%(numbers)s"),
-    "(chuck) [<n>]": Key("backspace:%(n)d"),
-
     "<specials> [repeat <n>]": Key("%(specials)s:%(n)d"),
-    "(slap|slop) [<n>]": Key("enter:%(n)d"),
-    "tab [<n>]": Key("tab:%(n)d"),
 }
 
 specialKeys = {
@@ -271,72 +213,17 @@ genericKeys = {
     "press shift": Key("shift:down"),
     "release shift": Key("shift:up"),
     "press <pressKey>": Key("%(pressKey)s"),
-
     "say <reservedWord>": Text("%(reservedWord)s"),
 
- # Navigation keys.
-    "page up [<n>]": Key("pgup:%(n)d"),
-    "page down [<n>]": Key("pgdown:%(n)d"),
-
-    "edit vim": Key("f12"),
-
+    # Navigation keys.
     "<modifier1> <pressKey> [<n>]":
-      Key("%(modifier1)s-%(pressKey)s:%(n)d"),
+        Key("%(modifier1)s-%(pressKey)s:%(n)d"),
     "<modifier1> <modifier2> <pressKey> [<n>]":
-      Key("%(modifier1)s%(modifier2)s-%(pressKey)s:%(n)d"),
+        Key("%(modifier1)s%(modifier2)s-%(pressKey)s:%(n)d"),
 
-    "add omni task": Key("ca-space"), # Add task with quick and entry to omnifocus
+    "add omni task": Key("ca-space"),  # Add task with quick and entry to omnifocus
     "tick": Mouse("left"),
     "dooble": Mouse("left:2"),
     "tock": Mouse("right"),
-    # "pick that": Key("casw-0"),
-    # "pock": Key("casw-9"),
     "dragon snore": Function(cancel_and_sleep),
-
-######################
-#    "launchbar": Key("w-space"),
-#    "switch apps": Key("win:down") + Key("tab"),
-#    "choose app": Key("win:up"),
-#
-#    "please quit that": Key("w-q"),
-#    "please close that": Key("w-w"),
-#    "hide that": Key("w-h"),
-#    "new that": Key("w-n"),
-#    "minimize that": Key("w-m"),
-#
-## app switching
-#    "iterm app": Key("caw-f1"),
-#    "terminal app": Key("caw-f2"),
-#    "PDF app": Key("caw-f3"),
-#    "mail-mate app": Key("caw-f4"),
-#    "chrome app": Key("caw-f5"),
-#    "calendar app": Key("caw-f6"),
-#    "finder app": Key("caw-f7"),
-#    "2-do app": Key("caw-f8"),
-#    "spotify app": Key("caw-f9"),
-#    "git-hub app": Key("caw-f10"),
-#    "1-password app": Key("caw-f11"),
-#    "zotero app": Key("caw-f12"),
-#    "are studio app": Key("scaw-f12"),
-#    "macvim app": Key("scaw-f11"),
-#    "preeve app": Key("caw-lbracket"),
-#    "next app": Key("caw-rbracket"),
-#
-## window management
-#    "window maximize": Key("asw-m"),
-#    "window half left": Key("caw-left"),
-#    "window half right": Key("caw-right"),
-#    "window bottom-left": Key("asw-left"),
-#    "window bottom-right": Key("asw-down"),
-#    "window top-left": Key("asw-up"),
-#    "window top-right": Key("asw-right"),
-#    "window [half] (center|middle)": Key("caw-down"),
-#    "window third (middle|center)": Key("asw-t"),
-#    "window third left": Key("asw-l"),
-#    "window third right": Key("asw-r"),
-#
-## folder and file opening
-#    "show downloads": Key("scaw-f1"),
-#
-#    "toggle dock": Key("aw-d"),
 }
