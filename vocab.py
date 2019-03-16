78e6e0bbdaff65f31fd443019cf65279c283f031
Maxim Danilov <danilov.ms.dev@gmail.com>
2019-03-16 13:27:04 +0500

    add(grammars): bdd, sequelize, environment

--- vocab.py
+++ vocab.py
@@ -1,10 +1,21 @@
 from aenea import (
-    Key,
     Text,
 )
 
 vocabWord = {
-    "change dir": Key("c, d"),
     "async": Text("async"),
     "await": Text("await"),
+
+    "test expect": Text("expect().to.equal()"),
+    "test subset": Text("expect().to.containSubset()"),
+
+    "todo": Text("// TODO: "),
+    "environment": Text("NODE_ENV"),
+
+    "fulfilment order": Text("fulfilmentOrder"),
+    "fulfilment": Text("fulfilment"),
+    "sequelize find all": Text("const r = await models..findAll({where: {}}))"),
+    "sequelize find one": Text("const r = await models..findOne({where: {}}))"),
+    "sequelize find by id": Text("const r = await models..findById()"),
+    #"": Text(""),
 }
