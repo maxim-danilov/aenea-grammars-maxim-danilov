13379a2c35ff87d71c4a9346319ee7f4be120513
Maxim Danilov <danilov.ms.dev@gmail.com>
2019-05-17 20:11:55 +0500

    add(grammars): vocabulary

--- vocab.py
+++ vocab.py
@@ -8,6 +8,18 @@ vocabWord = {
 
     "test expect": Text("expect().to.equal()"),
     "test subset": Text("expect().to.containSubset()"),
+    "test describe": Text("describe('', async () => { });"),
+    "test it": Text("it('', async () => { });"),
+    "test length": Text("expect().lengthOf()"),
+
+    # "": Text(""),
+
+    "partner": Text("fulfilmentPartner"),
+    "base test records": Text("baseTestRecords"),
+    "partner token": Text("fulfilmentPartnerToken"),
+    "partner user": Text("fulfilmentPartnerUser"),
+    "supplier user": Text("supplierUser"),
+    "supplier token": Text("supplierToken"),
 
     "todo": Text("// TODO: "),
     "environment": Text("NODE_ENV"),
@@ -17,5 +29,11 @@ vocabWord = {
     "sequelize find all": Text("const r = await models..findAll({where: {}}))"),
     "sequelize find one": Text("const r = await models..findOne({where: {}}))"),
     "sequelize find by id": Text("const r = await models..findById()"),
+
     #"": Text(""),
+
+    "constant": Text("coe"),
+    "arrow function": Text("afb"),
+
+    "swagger": Text("swagger"),
 }
